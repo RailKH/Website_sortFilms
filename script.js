535c9ac72e5a229e92eb2388d1a53cb3bc380c54
RailKH <55952216+RailKH@users.noreply.github.com>
2020-05-15 11:29:32 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,280 +1,225 @@
-const movieBlock = document.querySelector(".block-select .movie");
-const block_select = document.querySelector(".main .block-select");
-const favBlock = document.querySelector(".favorites .movie");
-const loadMore = document.querySelectorAll(".loadMore");
-const btnMove = document.querySelector(".btn-move");
-const btnFav = document.querySelector(".btn-fav");
-const searchBlock = document.querySelector(".search-wrapper_input input");
-const inputs = document.querySelectorAll(".tag .tag_input");
-let tags = [];
-let cinema = [];
+const movieBlock = document.querySelector(".movie-block");
+const favBlock = document.querySelector(".fav-block");
+const inputSearch = document.querySelector(".search-wrapper_input input");
+const searchWrapper = document.querySelector(".search-wrapper");
+const inputTagsBlock = document.querySelector(".tag");
+const loadMoreBtn = document.querySelector(".loadMore");
+const selectBlockBtn = document.querySelector(".select");
+const selectFilmsBlock = document.querySelector(".sel-film-block");
+
+let favCinema = JSON.parse(localStorage.getItem("fav")) || [];
+
 const maxItems = 15;
-const btn_cinema = document.querySelector(".block-select .loadMore");
-const btn_fav = document.querySelector(".favorites .loadMore");
-let itemsFilms = [];
-let favArray = JSON.parse(localStorage.getItem("fav")) || [];
 
-// Обработчик на кнопку ФИЛЬМЫ
-btnMove.addEventListener("click", e => {
-  draw();
-  if (!e.target.classList.contains("active")) {
+let cinema = [];
+let tags = [];
+let sortFilms = [];
 
-    btnFav.classList.remove("active");
-    btnMove.classList.add("active");
+const getData = async function (url) {
+    const response = await fetch(url);
 
-    block_select.classList.remove("hidden");
-    favBlock.classList.add("hidden");
-  }
-});
-
-// Функция получения списка с фильмами
-function getFilms() {
-  fetch("./jsons/films.json")
-    .then(res => res.json())
-    .then(data => {
-      cinema = data.filter(elem => {
-        return true;
-      });
-
-      createMovieBlock(data);
-      hideElem();
-      getTags();
-      locStorFilms();
-    });
-}
-getFilms();
-
-// Функция получения списка с тегами
-const getTags = () => {
-  fetch("./jsons/tags.json")
-    .then(res => res.json())
-    .then(data => {
-      tags = data.filter(elem => {
-        return true;
-      });
-    });
+    if (!response.ok) {
+        throw new Error(`Error ${url}, status ${response.status}`);
+    }
+    return await response.json();
 };
 
-// Функция заполнения страницы
-function createMovieBlock(data) {
-  data.forEach(elem => {
-    const div = document.createElement("div");
-    div.classList.add("card-film");
-
-    div.innerHTML = `<p data-tag="${elem.tags}">${elem.title}</p><div class="box"></div>`;
-
-    divMaker(div);
-  });
-  itemsFilms = document.querySelectorAll(".block-select .card-film");
-  draw();
-}
-
-// Функция внесение блока на страницу
-function divMaker(elem) {
-  movieBlock.append(elem);
-}
-
-// Функция отрисовки иконки в закладках
-function draw() {
-  itemsFilms.forEach(elem => {
-    elem.lastChild.classList.remove("fav");
-
-    favArray.forEach(item => {
-      if (elem.firstChild.textContent == item) {
-        elem.lastChild.classList.add("fav");
-      }
-       });
-  });
-}
-
-// Функция скрытия элементов
-function hideElem() {
-  const items = document.querySelectorAll(
-      ".block-select .card-film:not(.hidden2)"
-    ),
-    itemsFav = document.querySelectorAll(".favorites .card-film"),
-    hidenClass = "hidden";
-  if (items.length <= maxItems) {
-    btn_cinema.style.display = "none";
-  } else {
-    btn_cinema.style.display = "";
-  }
-  if (itemsFav.length <= maxItems) {
-    btn_fav.style.display = "none";
-  } else {
-    btn_fav.style.display = "";
-  }
-  items.forEach((item, ind) => {
-    if (ind > maxItems - 1) {
-      item.classList.add(hidenClass);
+function checkFavFilms(favFilm, event) {
+    const item = favCinema.find((item) => {
+        return item.title == favFilm;
+    });
+    if (item) {
+        favCinema.splice(
+            favCinema.findIndex((item) => item.title == favFilm),
+            1
+        );
+        event.target.classList.remove("select-fav-film");
     } else {
-      item.classList.remove(hidenClass);
+        favCinema.push({ title: favFilm, fav: true });
+        event.target.classList.add("select-fav-film");
     }
-  });
- 
-  itemsFav.forEach((item, ind) => {
-    if (ind > maxItems - 1) {
-      item.classList.add(hidenClass);
+}
+
+function addToFav(event) {
+    if (event.target.classList.contains("fav-box")) {
+        const favFilm = event.target.previousElementSibling.textContent;
+        checkFavFilms(favFilm, event);
+        localStorage.setItem("fav", JSON.stringify(favCinema));
+        if (event.target.closest(".fav-block")) {
+            event.target.parentElement.remove();
+            hideElements("fav-block");
+        }
+    }
+}
+
+function getFavBlock() {
+    cinema.forEach((item) => (item.fav = false));
+    if (favCinema) {
+        favCinema.forEach((item) => {
+            cinema.forEach((value) => {
+                if (item.title == value.title) {
+                    value.fav = true;
+                }
+            });
+        });
+    }
+}
+
+function createCardFilm(cinema) {
+    const { title, tags, fav } = cinema;
+    const value = fav ? "select-fav-film" : "";
+    const cinema_block = `
+      <div class="card-film"><p data-tag="${tags}">${title}</p><div class="fav-box ${value}"></div></div>`;
+    if (tags) {
+        movieBlock.insertAdjacentHTML("beforeend", cinema_block);
     } else {
-      item.classList.remove(hidenClass);
+        favBlock.insertAdjacentHTML("beforeend", cinema_block);
     }
-  });
 }
 
-// Обработчик кнопки Показать еще
-loadMore.forEach(elem => {
-  elem.addEventListener("click", e => {
-    openElem(e);
-  });
-});
+function selectBlock(event) {
+    if (event.target.classList.contains("btn-select")) {
+        if (!event.target.classList.contains("active")) {
+            for (let elem of selectBlockBtn.children) {
+                elem.classList.toggle("active");
+            }
 
-// Функция отображение скрытых элементов
-function openElem(e) {
-  const hidenClass =
-    "." + e.target.parentNode.className + " .hidden:not(.hidden2)";
-  const hidden = document.querySelectorAll(hidenClass);
+            for (let elem of selectFilmsBlock.children) {
+                elem.classList.toggle("hidden");
+            }
 
-  hidden.forEach((item, ind) => {
-    if (ind < maxItems) {
-      item.classList.remove("hidden");
+            if (event.target.classList.contains("favorites")) {
+                favBlock.innerHTML = "";
+                favCinema.forEach(createCardFilm);
+                hideElements("fav-block");
+                searchWrapper.classList.add("hidden");
+            } else {
+                movieBlock.innerHTML = "";
+                getFavBlock();
+                cinema.forEach(createCardFilm);
+                hideElements("movie-block");
+                searchWrapper.classList.remove("hidden");
+            }
+        }
     }
-  });
-  if (document.querySelectorAll(hidenClass).length == 0) {
-    e.target.style.display = "none";
-  }
 }
 
-// Функция поиска фильма по названию
-searchBlock.addEventListener("input", e => {
-  const searchText = new RegExp("^" + e.target.value.trim(), "i");
-  movieBlock.innerHTML = "";
+function searchFilms(event) {
+    movieBlock.innerHTML = "";
+    const target = event.target;
+    const searchText = new RegExp("^" + target.value.trim(), "i");
 
-  itemsFilms.forEach(card => {
-    const title = card.querySelector("p");
+    const films = cinema.filter((item) => {
+        return searchText.test(item.title);
+    });
 
-    if (searchText.test(title.textContent)) {
-      divMaker(card);
-    }
-  });
-  hideElem();
-});
+    films.forEach(createCardFilm);
+    sortFilms = films.slice();
+    sortWithTags();
+}
 
-// Функция поиска фильма по тегу
-inputs.forEach(elem => {
-  const drop_down = elem.nextElementSibling;
-
-  elem.addEventListener("input", () => {
+function createDropDown(event) {
+    const target = event.target;
+    const drop_down = target.nextElementSibling;
     drop_down.textContent = "";
-    if (elem.value === "") {
-      sortTags(elem.value);
-      return;
+
+    if (target.value === "") {
+        sortWithTags();
+        return;
     }
-    const filterTags = tags.filter(item => {
-      const fixItem = item.toLowerCase();
-      return fixItem.includes(elem.value.toLowerCase());
-    });
-    filterTags.forEach(item => {
-      const li = document.createElement("li");
-      li.classList.add("drop");
-      li.textContent = item;
-      drop_down.append(li);
+    const filterTags = tags.filter((item) => {
+        const fixItem = item.toLowerCase();
+        return fixItem.includes(target.value.toLowerCase());
     });
 
-    drop_down.addEventListener("click", e => {
-      if (e.target.tagName.toLowerCase() === "li") {
-        elem.value = e.target.textContent;
-        drop_down.textContent = "";
-      }
-      sortTags(elem.value);
+    filterTags.forEach((item) => {
+        const li = `<li class='drop'>${item}</li>`;
+        drop_down.insertAdjacentHTML("beforeend", li);
     });
-  });
-});
 
-// Функция сортировки по тегу
-function sortTags(tag) {
-  const items = document.querySelectorAll(".block-select .card-film");
-  const inputTags = document.querySelectorAll(".tag .tag_input");
-
-  items.forEach(elem => {
-    elem.classList.remove("hidden2");
-  });
-  inputTags.forEach(elem => {
-    drawByTag(elem);
-  });
-  hideElem();
+    drop_down.addEventListener("click", (e) => {
+        if (e.target.tagName.toLowerCase() === "li") {
+            target.value = e.target.textContent;
+            drop_down.textContent = "";
+        }
+        sortWithTags();
+    });
 }
 
-// Функция отрисовки по тегу
-function drawByTag(tag) {
-  const items = document.querySelectorAll(
-    ".block-select .card-film:not(.hidden2)"
-  );
-  items.forEach(item => {
-    if (!tag.value.length == 0) {
-      if (item.firstChild.getAttribute("data-tag").indexOf(tag.value) == -1) {
-        item.classList.add("hidden2");
-      }
+function sortWithTags() {
+    const inputTags = document.querySelectorAll(".tag .tag_input");
+    let films = sortFilms.slice();
+
+    movieBlock.innerHTML = "";
+    inputTags.forEach((tag) => {
+        if (tag.value) {
+            films.forEach((item, ind) => {
+                if (!item.tags.includes(tag.value)) {
+                    delete films[ind];
+                }
+            });
+        }
+    });
+
+    films.forEach(createCardFilm);
+    hideElements("movie-block");
+}
+
+function hideElements(valueBlocks) {
+    const cardFilm = document.querySelectorAll(`.${valueBlocks} .card-film`);
+    cardFilm.forEach((item, ind) => {
+        if (ind > maxItems - 1) {
+            item.classList.add("hidden");
+        } else item.classList.remove("hidden");
+    });
+    checkHideElements(valueBlocks);
+}
+
+function checkHideElements(valueBlocks) {
+    const films = document.querySelectorAll(`.${valueBlocks} .hidden`);
+
+    if (films.length == 0) {
+        loadMoreBtn.style.display = "none";
+    } else {
+        loadMoreBtn.style.display = "";
     }
-  });
-}
-// Функция работы с LocalStorage в блоке с фильмами
-function locStorFilms() {
-  itemsFilms.forEach(elem => {
-    elem.lastChild.addEventListener("click", e => {
-      if (e.target.classList.contains("fav")) {
-        e.target.classList.remove("fav");
-        favArray.splice(
-          favArray.indexOf(e.target.previousSibling.textContent),1
-        );
-        localStorage.setItem("fav", JSON.stringify(favArray));
-      } else {
-        e.target.classList.add("fav");
-        favArray.push(elem.firstChild.textContent);
-        localStorage.setItem("fav", JSON.stringify(favArray));
-      }
-    });
-  });
 }
 
-// Обработчик на кнопку ИЗБРАННЫЕ
-btnFav.addEventListener("click", e => {
-  if (!e.target.classList.contains("active")) {
-    btnFav.classList.add("active");
-    btnMove.classList.remove("active");
+function openElements(event) {
+    let valueBlocks = "";
 
-    block_select.classList.add("hidden");
-    favBlock.classList.remove("hidden");
-    favBlock.innerHTML = "";
+    for (let elem of selectFilmsBlock.children) {
+        if (!elem.classList.contains("hidden")) {
+            valueBlocks = elem.className;
+        }
+    }
 
-    favArray.forEach(elem => {
-      const div = document.createElement("div");
-      div.classList.add("card-film");
-      div.innerHTML = `<p>${elem}</p><div class="box fav"></div>`;
-      favBlock.append(div);
+    const hiddenElements = document.querySelectorAll(`.${valueBlocks} .hidden`);
+    hiddenElements.forEach((item, ind) => {
+        if (ind < maxItems) {
+            item.classList.remove("hidden");
+        }
     });
-
-    locStorFav();
-  }
-});
-
-// Функция работы с LocalStorage в блоке избранные
-function locStorFav() {
-  const itemsFav = document.querySelectorAll(".favorites .card-film");
-  hideElem();
-  itemsFav.forEach(elem => {
-    elem.lastChild.addEventListener("click", e => {
-      if (e.target.classList.contains("fav")) {
-        e.target.classList.remove("fav");
-        favArray.splice(
-          favArray.indexOf(e.target.previousSibling.textContent),
-          1
-        );
-        localStorage.setItem("fav", JSON.stringify(favArray));
-      } else {
-        e.target.classList.add("fav");
-        favArray.push(elem.firstChild.textContent);
-        localStorage.setItem("fav", JSON.stringify(favArray));
-      }
-    });
-  });
+    checkHideElements(valueBlocks);
 }
+
+async function init() {
+    await getData("./jsons/films.json").then((data) => {
+        cinema = data.slice();
+        sortFilms = data.slice();
+        getFavBlock();
+        cinema.forEach(createCardFilm);
+    });
+    getData("./jsons/tags.json").then((data) => {
+        tags = data.slice();
+    });
+    hideElements("movie-block");
+    inputSearch.addEventListener("input", searchFilms);
+    inputTagsBlock.addEventListener("input", createDropDown);
+    loadMoreBtn.addEventListener("click", openElements);
+    movieBlock.addEventListener("click", addToFav);
+    favBlock.addEventListener("click", addToFav);
+    selectBlockBtn.addEventListener("click", selectBlock);
+}
+
+init();
